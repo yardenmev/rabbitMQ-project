e243446c60a180ab9e5cec9ed02d22594c12f442
yardenmev <yardenmev@gmail.com>
2023-03-05 14:05:29 +0000

    topic

---
+++ topic-producer.py
@@ -0,0 +1,14 @@
+import pika
+from faker import Faker
+
+fake = Faker('iw_IL')
+connection_parameters = pika.ConnectionParameters(host='localhost')
+with pika.BlockingConnection(connection_parameters) as connection:
+    channel = connection.channel()
+    channel.exchange_declare(exchange='MyExchange' , exchange_type='topic')
+    for x in range(1000):
+        person = fake.name()
+        location = fake.city()
+        message = f"{person} is in city {location}"
+        channel.basic_publish(exchange='MyExchange',routing_key=location ,body=message.encode('utf-8'))
+
