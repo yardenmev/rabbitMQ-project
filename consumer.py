743c4564c0be1cc2c3843089b69c630d89b85e95
yardenmev <yardenmev@gmail.com>
2023-03-05 12:48:52 +0000

    looking for or haner

--- consumer.py
+++ consumer.py
@@ -1,6 +1,7 @@
 import pika
 def callback(ch, method, properties, body):
-    print(f"Got message: {body.decode('utf-8')}")   
+    if "אור הנר" in body.decode('utf-8'):
+        print(f"Got message: {body.decode('utf-8')}")  
 
 connection_parameters = pika.ConnectionParameters(host='localhost')
 with pika.BlockingConnection(connection_parameters) as connection:
@@ -9,4 +10,5 @@ with pika.BlockingConnection(connection_parameters) as connection:
     channel.queue_declare(queue='MyQueue')
     channel.queue_bind(exchange='MyExchange', queue='MyQueue')
     channel.basic_consume(queue='MyQueue',on_message_callback=callback,auto_ack=True)
-    channel.start_consuming()
+    channel.start_consuming() 
+    
