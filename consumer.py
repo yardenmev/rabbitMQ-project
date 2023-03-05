bb401f686774fdc521465723a1927a2d5e5e98d5
yardenmev <yardenmev@gmail.com>
2023-03-05 10:52:37 +0000

    fanout

--- consumer.py
+++ consumer.py
@@ -5,5 +5,8 @@ def callback(ch, method, properties, body):
 connection_parameters = pika.ConnectionParameters(host='localhost')
 with pika.BlockingConnection(connection_parameters) as connection:
     channel = connection.channel()
-    channel.basic_consume(queue='person-data',on_message_callback=callback,auto_ack=True)
+    channel.exchange_declare(exchange='MyExchange', exchange_type='fanout')
+    channel.queue_declare(queue='MyQueue')
+    channel.queue_bind(exchange='MyExchange', queue='MyQueue')
+    channel.basic_consume(queue='MyQueue',on_message_callback=callback,auto_ack=True)
     channel.start_consuming()
--- fanout-producer.py
+++ fanout-producer.py
@@ -5,11 +5,12 @@ from faker import Faker
 connection_parameters = pika.ConnectionParameters(host='localhost')
 with pika.BlockingConnection(connection_parameters) as connection:
     channel = connection.channel()
-    channel.queue_declare(queue='person-data')
+    channel.exchange_declare(exchange='MyExchange', exchange_type='fanout')
+    # channel.queue_declare(queue='person-data')
     for x in range(100):
         fake = Faker('iw_IL')
         person = fake.name()
         location = fake.city()
         message = f"{person} is in city {location}"
-        channel.basic_publish(exchange='',routing_key='person-data',body=message.encode('utf-8'))
+        channel.basic_publish(exchange='MyExchange',routing_key='',body=message.encode('utf-8'))
 
