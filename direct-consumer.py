50209f059f59c41b27bb0b47c16867eb04dde4c9
yardenmev <yardenmev@gmail.com>
2023-03-05 13:42:19 +0000

    DIRECT

--- direct-consumer.py
+++ direct-consumer.py
@@ -1,14 +1,13 @@
 import pika
 def callback(ch, method, properties, body):
-    if "אור הנר" in body.decode('utf-8'):
-        print(f"Got message: {body.decode('utf-8')}")  
+    print(f"Got message: {body.decode('utf-8')}")  
 
 connection_parameters = pika.ConnectionParameters(host='localhost')
 with pika.BlockingConnection(connection_parameters) as connection:
     channel = connection.channel()
-    channel.exchange_declare(exchange='MyExchange', exchange_type='fanout')
+    channel.exchange_declare(exchange='MyExchange',exchange_type='direct' )
     channel.queue_declare(queue='MyQueue')
-    channel.queue_bind(exchange='MyExchange', queue='MyQueue',)
+    channel.queue_bind(exchange='MyExchange', queue='MyQueue',routing_key='אור הנר')
     channel.basic_consume(queue='MyQueue',on_message_callback=callback,auto_ack=True)
     channel.start_consuming() 
     
