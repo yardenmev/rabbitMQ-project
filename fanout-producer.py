18c305f86183b7551ddd4413d8d80f6a95cf62c8
yardenmev <yardenmev@gmail.com>
2023-03-05 10:35:52 +0000

    fan out

---
+++ fanout-producer.py
@@ -0,0 +1,15 @@
+import pika
+from faker import Faker
+
+
+connection_parameters = pika.ConnectionParameters(host='localhost')
+with pika.BlockingConnection(connection_parameters) as connection:
+    channel = connection.channel()
+    channel.queue_declare(queue='person-data')
+    for x in range(100):
+        fake = Faker('iw_IL')
+        person = fake.name()
+        location = fake.city()
+        message = f"{person} is in city {location}"
+        channel.basic_publish(exchange='',routing_key='person-data',body=message.encode('utf-8'))
+
